295beb76ac210604c5c1f9da55d46c3a1449b04b
Fagner Cunha da Cruz <fagnerccruz@gmail.com>
2023-07-26 19:17:36 -0300

    React hook form login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,18 @@
 import "./login.css";
 import { ReactComponent as HeroImg } from "../../assets/images/Desenho.svg";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  email: string;
+  senha: string;
+};
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="login-container">
@@ -12,23 +23,28 @@ const Login = () => {
             <HeroImg />
           </div>
         </div>
+
         <div className="login-card efeito-glass bordas-arredondadas">
-          <h3>LOGIN</h3>
-          <div className="campos-form">
-            <input
-              type="text"
-              id="email"
-              name="email"
-              className="form-control"
-            />
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              className="form-control"
-            />
-            <button className="btn btn-primary">FAZER LOGIN</button>
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h3>LOGIN</h3>
+            <div className="campos-form">
+              <input
+                {...register("email")}
+                type="text"
+                id="email"
+                name="email"
+                className="form-control"
+              />
+              <input
+                {...register("senha")}
+                type="password"
+                id="senha"
+                name="senha"
+                className="form-control"
+              />
+              <button className="btn btn-primary">FAZER LOGIN</button>
+            </div>
+          </form>
         </div>
       </div>
     </>
