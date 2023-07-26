644ada73c0939a328e4500f14066cf9d09c77cf3
Fagner Cruz <fagner@sti.ufpb.br>
2023-07-26 12:40:49 -0300

    Ajustes nos componentes e no layout

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./movies.css";
 
 const Movies = () => {
@@ -5,8 +6,12 @@ const Movies = () => {
     <>
       <div className="movies-container">
         <h2>Tela listagem de filmes</h2>
-        <p>Acessar /movies/1</p>
-        <p>Acessar /movies/2</p>
+        <Link to="/movies/1" style={{textDecoration: 'none', color: 'unset'}}>
+          <p>Acessar /movies/1</p>
+        </Link>
+        <Link to="/movies/2" style={{textDecoration: 'none', color: 'unset'}}>
+          <p>Acessar /movies/2</p>
+        </Link>
       </div>
     </>
   );
--- src/pages/MoviesDetails/index.tsx
+++ src/pages/MoviesDetails/index.tsx
@@ -5,7 +5,7 @@ import ReviewList from "components/ReviewList";
 const MoviesDetails = () => {
   return (
     <>
-      <div className="movies-container">
+      <div className="moviesd-container">
         <div className="title-order">
           <h2>Tela detalhes do filme</h2>
           <p className="id-filme">id: 1</p>
